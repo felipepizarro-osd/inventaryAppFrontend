b95cb2a73455f222e1d273e20444b9046eda9d0d
MarceloAGD <marcelo.guerra@alumnos.ucn.cl>
2022-06-07 13:32:20 -0400

    Search product update

--- src/components/searchProduct/searchProducts.jsx
+++ src/components/searchProduct/searchProducts.jsx
@@ -1,31 +1,139 @@
-import React from 'react'
+import React, {useState } from 'react';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import './searchProducts.scss'
-
+import axios from 'axios';
+import { Modal } from '@mui/material';
+//import { flexbox } from '@mui/system';
 const tab = '\u00A0'
 
-function searchProducts() {
-  return (
-    <div className='Buscar'>
-        <div className='top'>
-            <div className='title'>
-                Sistema de Inventario
-            </div>
-        </div>
+export default function SearchProducts() {
+    const [open, setOpen] = useState(false);
 
-        <div className='camera'>
-            Camara
-            <div className='frame'>
-                <CameraAltIcon className='cameraIcon'/>
-            </div>
+    const handleClickOpen = () => {
+      if(dato.sku[0] === ''){
+        setOpen(false);
+      }else{setOpen(true)}
+      
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+    
+    const [product, setProduct] = useState([]);
+    const searchProduct = (sku) => {
+        console.log(sku)
+        axios.get(`http://localhost:4000/api/products/${sku}`).then((response) => {
+        console.log(response.data);
+        setProduct(response.data);
+    });
+    }
+    const [dato, setDato] = useState({sku: ''})
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+        setDato({
+            ...dato,
+            [event.target.name] : [event.target.value]
+        })
+
+    };
+
+    const enviarDato = (event) => {
+        event.preventDefault();
+        searchProduct(dato.sku[0])
+    };
+    
+    const useStyles = {
+          position: 'absolute',
+          padding: "12px 12px 12px",
+          backgroundColor: 'white',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          borderRadius: '20px'
+          
+      }
+    const prodStyles={
+      borderBottom: '6px', padding:'4px'
+    }
+    const colorStyles={
+      backgroundColor:'#1b5a74',
+      color:'white',
+
+    }
+    const body=(
+      <div style={useStyles}>
+        <div align='left'>
+          <h2>Producto buscado</h2>
+          <p style={prodStyles}>Sku: </p>
+          <p style={colorStyles}>{product.Sku}</p>
+        
+          <p style={prodStyles}>Nombre: </p>
+          <p style={colorStyles}>{product.Nombre}</p>
+          
+          <p style={prodStyles}>Nombre de servicio: </p>
+          <p style={colorStyles}>{product.Nombre_Servicio}</p>
+
+          <p style={prodStyles}>Part number: </p>
+          <p style={colorStyles}>{product.Part_Number}</p>
+          
+          <p style={prodStyles}>Stock: </p>
+          <p style={colorStyles}>{product.Stock}</p>
+          
+          <p style={prodStyles}>Stock minimo: </p>
+          <p style={colorStyles}>{product.Stock_min}</p>
+ 
+          <p style={prodStyles}>Unidad: </p>
+          <p style={colorStyles}>{product.Unidad}</p>
+        
+        <div style={{padding:'10px'}}>
+          <button onClick={handleClose}> cerrar</button>
         </div>
-        <div className='formulario'>
-            <h1 className='titulo'>Buscar Producto por Qr</h1>{tab}
-            <input className='inputSku' type='text' placeholder='SKU del producto'></input>{tab}
-            <button className='button' >Buscar</button>
         </div>
     </div>
-  )
-}
+    )
+    const bodyAlert=(
+      <div style={useStyles}>
+        <h2>Producto no encontrado!!</h2>
+        <div style={{padding:'10px'}}>
+          <button className='cerrar' onClick={handleClose}> cerrar</button>
+        </div>
+      </div>
+    )
+    return (
+      <div className="Buscar">
+        <div className="top">
+          <div className="title">Sistema de Inventario</div>
+        </div>
 
-export default searchProducts
+        <div className="camera">
+          Camara
+          <div className="frame">
+            <CameraAltIcon className="cameraIcon" />
+          </div>
+        </div>
+        <div className="formulario">
+            <h1 className="titulo">Buscar Producto por Qr</h1>
+            {tab}
+            <form onSubmit={enviarDato}>
+                <input
+                    className='input-sku'
+                    type="text"
+                    placeholder="SKU del producto"
+                    name='sku'    
+                    onChange={handleChange}                                      
+                />
+                
+                {tab}
+                <button type="submit" className="button" onClick={handleClickOpen}>
+                    Buscar
+                </button>
+            
+            </form>
+            { dato.sku[0] !== product.Sku ||
+              <Modal  open={open} onClose={handleClickOpen}>{body}</Modal>
+            }{ dato.sku[0] === product.Sku || <Modal  open={open} onClose={handleClickOpen}>{bodyAlert}</Modal>}
+          </div>
+      </div>
+    )}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,38 +9,31 @@ import Chart from '../../components/chart/Chart'
 import Search from '../../components/searchProduct/searchProducts'
 import axios from 'axios';
 import Grid from '@mui/material/Grid'
-import DeleteIcon from '@mui/icons-material/Delete'
-import Edit from '@mui/icons-material/Edit'
-import IconButton from '@mui/material/IconButton'
+import TablaProducto from '../../components/table/tablaProducto';
 //import { render } from 'react-dom';
 
-function useDatos() {
-  const [products, setProducts] = useState([]);
 
+export default function Home() {
+  
+  const [products, setProducts] = useState([]);
+  
   useEffect(() => {
     fetch("http://localhost:4000/api/products")
     .then((res) => {
       if(res.ok) return res.json();
     })
     .then((jsonRes) => setProducts(jsonRes));
-  }, [])
-
-  return products;
-}
-
-function deleteProducts(sku){
-  if(window.confirm('Desea borrar este producto?')){
-
-    axios.delete(`http://localhost:4000/api/products/:${sku}`).then((response) => {
-      console.log(sku);
-    });
+    
+  },[])
+  
+  const deleteProducts = (sku) => {
+    console.log(sku)
+    if(window.confirm('Desea borrar este producto?')){
+      axios.delete(`http://localhost:4000/api/products/${sku}`)
+      }
+    const arrayFiltrado = products.filter(prod => prod.sku !== sku);
+    setProducts(arrayFiltrado)
   }
-}
-
-export default function Home() {
-  
-  const render = useDatos()
-  
   return (
       <div className="home">
         <Sidebar />
@@ -57,49 +50,14 @@ export default function Home() {
             <Featured />
             <Chart />
           </div>
-          <Grid container spacing={2} justifyContent="center" >
-            
+          <Grid container spacing={2} justifyContent="center" >            
             <div className="row">
-              <table className='tabla_producto'>
-                <thead>
-                  <tr>
-                    <th>Sku</th>
-                    <th>Nombre</th>
-                    <th>Nombre de servicio</th>
-                    <th>Part number</th>
-                    <th>Stock</th>
-                    <th>Stock minimo</th>
-                    <th>Unidad</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {render.map((products) => {
-                    return (
-                      <tr key={products.Sku}>
-                        <td>{products.Sku}</td>
-                        <td>{products.Nombre}</td>
-                        <td>{products.Nombre_Servicio}</td>
-                        <td>{products.Part_Number}</td>
-                        <td>{products.Stock}</td>
-                        <td>{products.Stock_min}</td>
-                        <td>{products.Unidad}</td>
-
-                        <td>
-                          <IconButton variant='outlined' aria-label="delete" onClick={() => deleteProducts(products.Sku)}>
-                            <DeleteIcon />
-                          </IconButton>
-                          <IconButton variant='outlined' aria-label="edit" >
-                            <Edit />
-                          </IconButton>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+              <TablaProducto products={products} deleteProducts={deleteProducts} />
             </div>
           </Grid>
         </div>
       </div>
+    
     );
+    
 }
