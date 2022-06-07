cc17cb71efe7869fd8ad1c629db187ddd7b60cc4
Felipe Pizarro <66143232+felipepizarro-osd@users.noreply.github.com>
2022-06-06 21:55:15 -0400

    second format

--- src/components/searchProduct/searchProducts.jsx
+++ src/components/searchProduct/searchProducts.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import './searchProducts.scss'
 function searchProducts() {
-  return (
-    <div className='Buscar'>
-        <div className='top'>
-            <div className='title'>
-                Sistema de Inventario
+    return (
+        <div className='Buscar'>
+            <div className='top'>
+                <div className='title'>
+                    Sistema de Inventario
+                </div>
             </div>
-        </div>
 
-        <div className='camera'>
-            Camara
-            <div className='frame'>
-                <CameraAltIcon className='cameraIcon'/>
+            <div className='container'>
+                <Card sx={{ maxWidth: 600 }}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                    />
+                    <CardContent className='contenido'>
+ 
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
             </div>
         </div>
-        <div className='formulario'>
-            <h1 className='titulo'>Buscar Producto por Qr</h1>
-            <input className='inputSku' type='text' placeholder='SKU del producto'></input>
-            <button className='button' >Buscar</button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default searchProducts
