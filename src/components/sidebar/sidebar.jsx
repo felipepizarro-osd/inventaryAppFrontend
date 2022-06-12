7d1e582483a240008b1aa6a4c47ea4152cf309b5
MarceloAGD <marcelo.guerra@alumnos.ucn.cl>
2022-06-12 16:24:28 -0400

    Update sidebar.jsx

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -5,26 +5,23 @@ import { Link } from "react-router-dom";
 import Navbar from "../navbar/navbar";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import logo from "..//img/Logo.png";
-
 const tab = "\u00A0";
 
 const Sidebar = () => {
-
-  const [show, setShow]= useState(false)
+  const [show, setShow] = useState(false);
 
   return (
     <main className={show ? "space-toggle" : null}>
       <header className={`header ${show ? "space-toggle" : null}`}>
-        <div className="header-toggle">
-          <ViewHeadlineIcon className="icon" onClick={() => setShow(!show)}/>
-          </div>
-          <div className="logo ">
+        <div className="header-toggle" onClick={() => setShow(!show)}>
+          <ViewHeadlineIcon className="icon" />    
+        </div>
+        <div className="logo ">
             <img src={logo} alt="logo" height={30} width={70} />
             <span className="logo-name">
               {tab}Software{tab}Inventory
             </span>
           </div>
-        
 
         <div className="nav-login">
           <Link to="" className="nav-link">
@@ -37,7 +34,7 @@ const Sidebar = () => {
         <Navbar/>
       </aside>
     </main>
-  )
-}
+  );
+};
 
 export default Sidebar;
