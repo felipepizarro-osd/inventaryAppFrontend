4cfc6a2b327048e8fb341129df8db22f2ecc8d0b
MarceloAGD <marcelo.guerra@alumnos.ucn.cl>
2022-06-13 21:08:44 -0400

    Update scanner

--- src/components/scannerQr/Scanner.jsx
+++ src/components/scannerQr/Scanner.jsx
@@ -1,28 +1,137 @@
 import React, { useState} from 'react';
 import { QrReader } from 'react-qr-reader';
+import axios from "axios";
+import { Modal, IconButton, Grid } from "@mui/material";
+import { QRCode } from "react-qr-svg";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Scanner = () => {
-  const [data, setData] = useState('No result');
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    if (data.sku[0] === "" || data.sku[0] === undefined) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [data, setData] = useState({ sku: "" });
   const [boton, setBoton] = useState(false);
-  const handleBoton = () => {setBoton(!boton)};
+
+  const handleBoton = () => {
+    setBoton(true);
+    navigator.mediaDevices
+    .getUserMedia({ video: true})
+  };
+  const handleCloseBoton =  () => {
+    setBoton(false);
+  };
+
+  const [product, setProduct] = useState({ Sku: "" });
+  const searchProduct = (sku) => {
+      axios.get(`http://localhost:4000/api/products/${sku}`).then((response) => {
+          setProduct(response.data);
+          
+          if(response.data !== undefined){
+              setOpen(true);
+          }
+      });
+      
+  }
+  const useStyles = {
+    position: "absolute",
+    padding: "12px 12px 12px",
+    backgroundColor: "white",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "20px",
+  };
+  const prodStyles = {
+    borderBottom: "6px",
+    padding: "4px",
+  };
+  const colorStyles = {
+    backgroundColor: "#1b5a74",
+    color: "white",
+  };
+  const body = (
+    <div className="body" style={useStyles}>
+      <Grid container justifyContent="space-between">
+        <Grid item>
+          <h2>Producto buscado</h2>
+        </Grid>
+        <Grid
+          item
+          alignItems="flex-start"
+          style={{ transform: "translate(30%, -20%)" }}
+        >
+          <IconButton size="large" onClick={handleClose}>
+            <CloseIcon fontSize="inherit" />{" "}
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item sm style={{ display: "block", margin: "auto" }}>
+          <QRCode value={product.Sku} level={"L"} style={{ width: 250 }} />
+        </Grid>
+
+        <Grid
+          item
+          sm
+          container
+          direction="column"
+          style={{ marginLeft: "10px", marginRight: "8px" }}
+        >
+          <p style={prodStyles}>Sku: </p>
+          <p style={colorStyles}>{product.Sku}</p>
+
+          <p style={prodStyles}>Nombre: </p>
+          <p style={colorStyles}>{product.Nombre}</p>
+
+          <p style={prodStyles}>Nombre de servicio: </p>
+          <p style={colorStyles}>{product.Nombre_Servicio}</p>
+
+          <p style={prodStyles}>Part number: </p>
+          <p style={colorStyles}>{product.Part_Number}</p>
+
+          <p style={prodStyles}>Stock: </p>
+          <p style={colorStyles}>{product.Stock}</p>
+
+          <p style={prodStyles}>Stock minimo: </p>
+          <p style={colorStyles}>{product.Stock_min}</p>
+
+          <p style={prodStyles}>Unidad: </p>
+          <p style={colorStyles}>{product.Unidad}</p>
+        </Grid>
+      </Grid>
+    </div>
+  );
   return (
     <>
     <div style={{width:'250px'}}>
       <div>
-      <button className="button" onClick={handleBoton}>Camara</button>
+      <button className="button" onClick={handleBoton}>Activar Camara</button>
+      <button className="button" onClick={handleCloseBoton}>Desactivar Camara</button>
        </div>
        {boton === true && <QrReader 
         delay={300}
         onResult={(result) => {
-          if(boton === true){
             if (!!result) {
             setData(result?.text);
+            searchProduct(result?.text);
           }
-        }
         }}
         style={{ width: '100%' }}
       />}
-      <p>{data}</p>
+      
+      <Modal open={open} onClose={handleClickOpen} >
+        {body}
+      </Modal>
+      
       </div>
     </>
   );
--- src/components/searchProduct/searchProducts.jsx
+++ src/components/searchProduct/searchProducts.jsx
@@ -1,161 +1,186 @@
-import React, {useState } from 'react';
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import './searchProducts.scss'
-import axios from 'axios';
-import { Grid, IconButton, Modal } from '@mui/material';
-import {QRCode} from 'react-qr-svg';
-import CloseIcon from '@mui/icons-material/Close';
-import Scanner from '../scannerQr/Scanner';
-
-//import { flexbox } from '@mui/system';
-const tab = '\u00A0'
+import React, { useState } from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/material/Button";
+import "./searchProducts.scss";
+import axios from "axios";
+import { Modal, IconButton, Grid } from "@mui/material";
+import { QRCode } from "react-qr-svg";
+import CloseIcon from "@mui/icons-material/Close";
+import Scanner from "../scannerQr/Scanner";
 
 export default function SearchProducts() {
+  const [open, setOpen] = useState(false);
 
-
-    const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-      if(dato.sku[0] === '' || dato.sku[0]=== undefined){
-        setOpen(false);
-      }else{setOpen(true)}
-      
-    };
-
-    const handleClose = () => {
+  const handleClickOpen = () => {
+    if (dato.sku[0] === "" || dato.sku[0] === undefined) {
       setOpen(false);
-    };
-    
-    const [product, setProduct] = useState({Sku: ''});
-    const searchProduct = (sku) => {
-        console.log(sku)
-        axios.get(`http://localhost:4000/api/products/${sku}`).then((response) => {
-        console.log(response.data);
-        setProduct(response.data);
+    } else {
+      setOpen(true);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [product, setProduct] = useState({ Sku: "" });
+  const searchProduct = (sku) => {
+    axios.get(`http://localhost:4000/api/products/${sku}`).then((response) => {
+      setProduct(response.data);
     });
-    }
-    const [dato, setDato] = useState({sku: ''})
+  };
+  const [dato, setDato] = useState({ sku: "" });
 
-    const handleChange = (event) => {
-        console.log(event.target.value)
-        setDato({
-            ...dato,
-            [event.target.name] : [event.target.value]
-        })
+  const handleChange = (event) => {
+    setDato({
+      ...dato,
+      [event.target.name]: [event.target.value],
+    });
+  };
 
-    };
+  const enviarDato = (event) => {
+    event.preventDefault();
+    searchProduct(dato.sku[0]);
+  };
 
-    const enviarDato = (event) => {
-        event.preventDefault();
-        searchProduct(dato.sku[0])
-    };
-    
-    const useStyles = {
-          position: 'absolute',
-          padding: "12px 12px 12px",
-          backgroundColor: 'white',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          borderRadius: '20px'
-          
-      }
-    const prodStyles={
-      borderBottom: '6px', padding:'4px',
-    }
-    const colorStyles={
-      backgroundColor:'#1b5a74',
-      color:'white',
-    }
-    const body=(
-      <div className='body' style={useStyles}>
-        <Grid container justifyContent="space-between">
-          <Grid item>
-            <h2>Producto buscado</h2>
-          </Grid>
-          <Grid item alignItems="flex-start" style={{transform: 'translate(30%, -20%)'}}>
-            <IconButton size='large' onClick={handleClose}><CloseIcon fontSize="inherit"/> </IconButton>
-          </Grid>
+  const useStyles = {
+    position: "absolute",
+    padding: "12px 12px 12px",
+    backgroundColor: "white",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "20px",
+  };
+  const prodStyles = {
+    borderBottom: "6px",
+    padding: "4px",
+  };
+  const colorStyles = {
+    backgroundColor: "#1b5a74",
+    color: "white",
+  };
+  const body = (
+    <div className="body" style={useStyles}>
+      <Grid container justifyContent="space-between">
+        <Grid item>
+          <h2>Producto buscado</h2>
         </Grid>
-        <Grid container>
-            
-          <Grid item sm style={{display:'block', margin:'auto'}}>
-            <QRCode value={product.Sku} level={'L'} style={{ width: 250 }}/>
-          </Grid>
-
-          <Grid item sm container direction='column' style={{marginLeft:'10px',marginRight:'8px'}}>
-            <p style={prodStyles}>Sku: </p>
-            <p style={colorStyles}>{product.Sku}</p>
-          
-            <p style={prodStyles}>Nombre: </p>
-            <p style={colorStyles}>{product.Nombre}</p>
-            
-            <p style={prodStyles}>Nombre de servicio: </p>
-            <p style={colorStyles}>{product.Nombre_Servicio}</p>
-
-            <p style={prodStyles}>Part number: </p>
-            <p style={colorStyles}>{product.Part_Number}</p>
-            
-            <p style={prodStyles}>Stock: </p>
-            <p style={colorStyles}>{product.Stock}</p>
-            
-            <p style={prodStyles}>Stock minimo: </p>
-            <p style={colorStyles}>{product.Stock_min}</p>
-  
-            <p style={prodStyles}>Unidad: </p>
-            <p style={colorStyles}>{product.Unidad}</p>
-          </Grid>
-          
+        <Grid
+          item
+          alignItems="flex-start"
+          style={{ transform: "translate(30%, -20%)" }}
+        >
+          <IconButton size="large" onClick={handleClose}>
+            <CloseIcon fontSize="inherit" />{" "}
+          </IconButton>
         </Grid>
-            
-            
+      </Grid>
+      <Grid container>
+        <Grid item sm style={{ display: "block", margin: "auto" }}>
+          <QRCode value={product.Sku} level={"L"} style={{ width: 250 }} />
+        </Grid>
+
+        <Grid
+          item
+          sm
+          container
+          direction="column"
+          style={{ marginLeft: "10px", marginRight: "8px" }}
+        >
+          <p style={prodStyles}>Sku: </p>
+          <p style={colorStyles}>{product.Sku}</p>
+
+          <p style={prodStyles}>Nombre: </p>
+          <p style={colorStyles}>{product.Nombre}</p>
+
+          <p style={prodStyles}>Nombre de servicio: </p>
+          <p style={colorStyles}>{product.Nombre_Servicio}</p>
+
+          <p style={prodStyles}>Part number: </p>
+          <p style={colorStyles}>{product.Part_Number}</p>
+
+          <p style={prodStyles}>Stock: </p>
+          <p style={colorStyles}>{product.Stock}</p>
+
+          <p style={prodStyles}>Stock minimo: </p>
+          <p style={colorStyles}>{product.Stock_min}</p>
+
+          <p style={prodStyles}>Unidad: </p>
+          <p style={colorStyles}>{product.Unidad}</p>
+        </Grid>
+      </Grid>
     </div>
-    )
-    const bodyAlert=(
-      <div style={useStyles}>
-        <h2>Producto no encontrado!!</h2>
-        <div style={{padding:'10px'}}>
-          <button className='cerrar' onClick={handleClose}> cerrar</button>
-        </div>
+  );
+  const bodyAlert = (
+    <div style={useStyles}>
+      <h2>Producto no encontrado!!</h2>
+      <div style={{ padding: "10px" }}>
+        <button className="cerrar" onClick={handleClose}>
+          {" "}
+          cerrar
+        </button>
       </div>
-    )
-    return (
-      <div className="Buscar">
-        <div className="top">
-          <div className="title">Sistema de Inventario</div>
-        </div>
+    </div>
+  );
+  return (
+    <div className="Buscar">
+      <div className="top">
+        <div className="title">Sistema de Inventario</div>
+      </div>
 
-        <div className="camera">
-          Camara
-          <div className="frame">
-            <CameraAltIcon className="cameraIcon" />
-            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-              <Scanner/>
-            </Grid>
-          </div>
-            
+      <div className="container">
+        <div className="info">
+          <h4>Buscar Producto por QR</h4>
+          <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+            <Scanner />
+          </Grid>
+          <form onSubmit={enviarDato}>
+            <FormControl fullWidth sx={{ m: 0.5 }}>
+              <InputLabel className="input" htmlFor="outlined-adornment-amount">
+                SKU del Producto
+              </InputLabel>
+              <OutlinedInput
+                //id="outlined-adornment-amount"
+                style={{
+                  borderRadius: "20px",
+                  width: "100%",
+                  marginBottom: "20px",
+                  backgroundColor: "#1B5A74",
+                  borderColor: "white",
+                }}
+                color="success"
+                label="Sku del Producto"
+                onChange={handleChange}
+                name="sku"
+                type="text"
+              />
+            </FormControl>
+            <div className="bton">
+              <Button
+                type="submit"
+                size="large"
+                variant="contained"
+                onClick={handleClickOpen}
+              >
+                Buscar
+              </Button>
+            </div>
+          </form>
+          {dato.sku[0] === product.Sku && (
+            <Modal open={open} onClose={handleClickOpen}>
+              {body}
+            </Modal>
+          )}
+          {dato.sku[0] !== product.Sku && (
+            <Modal open={open} onClose={handleClickOpen}>
+              {bodyAlert}
+            </Modal>
+          )}
         </div>
-        <div className="formulario">
-            <h1 className="titulo">Buscar Producto por Qr</h1>
-            {tab}
-            <form onSubmit={enviarDato}>
-                <input
-                    className='input-sku'
-                    type="text"
-                    placeholder="SKU del producto"
-                    name='sku'    
-                    onChange={handleChange}
-                />
-                
-                {tab}
-                <button type="submit" className="button" onClick={handleClickOpen}>
-                    Buscar
-                </button>
-            
-            </form>
-            { dato.sku[0] === product.Sku &&
-              <Modal  className='modal' open={open} onClose={handleClose}>{body}</Modal>
-            }{ dato.sku[0] !== product.Sku && <Modal  open={open} onClose={handleClose }>{bodyAlert}</Modal>}
-          </div>
       </div>
-    )}
+    </div>
+  );
+}
