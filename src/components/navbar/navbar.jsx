07d33399e955f35b6512babbf423b8e77d96d26b
Diego Barros <diego.barros@alumnos.ucn.cl>
2022-07-15 05:24:32 -0400

    login avance 15/07

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import './navbar.scss'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Link } from 'react-router-dom';
+
+if(window.IsLogin){
+  console.log('esta logeado');
+}
+else{
+  console.log('no esta logeado');
+}
 function navbar() {
   return (
     <div className='navbar'>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,40 +5,63 @@ import Sidebar from '../../components/sidebar/sidebar'
 import Navbar from '../../components/navbar/navbar'
 import axios from 'axios'
 import "./Login.scss"
-import { FormControlUnstyled } from '@mui/base';
+//import { FormControlUnstyled } from '@mui/base';
 
+ 
+//function enviarDatos(user)
+//let logeado=false;
+//window.isLogin = false;
 
 const Login=()=> {
   const paperStyle={padding :20, height:'50vh',width:280, margin: "80px auto"}
   const avatarStyle={backgroundColor: '#33A5FF'}
   const btnstyle={margin:'8px 0', backgroundColor: '#EA454C'}
   const usuarioStyle={margin: "10px auto"}
-  const  [body,setBody] = useState({rut: '', contrasena: ''})
-
-  const inputChange = ({target}) =>{
-    const {name, value} = target
-    setBody({
-      ...body,
-      [name]: value
-    })
+  //const  [body,setBody] = useState({rut: '', contrasena: ''})
+  const [rut,setRut]=useState("")
+  const [contrasena,setContrasena]=useState("")
+  
+  const user = {
+    'rut':rut,
+    'contrasena':contrasena
   }
-  const onSubmit=(body)=>{
-    console.log(body)
-    axios.get(`http://localhost:4000/api/usuarios/${body.rut}`)
-    .then(({data})=>{
-      console.log(data[0]['Contrasena'])
-      if(data[0]['Contrasena'] === body.contrasena){
-        return(
-          console.log('Son iguales')
-        );
+  
+  const onSubmit=()=>{
+
+    const request = async () => {
+      try {
+        const result = await axios.post('http://localhost:4000/api/login',user);
+        return {
+          success: true,
+          data: result
+        }
+      } catch( error ) {
+        return {
+          success: false,
+          data: error
+        }
       }
-      console.log(data[0]['Rut'])
-    })
-    .catch(({response})=>{
-      FormControlUnstyled.log(response)
-    })
+    };
+    ( async() => {
+      const result = await request();
+      if ( result.success ) {
+        if(result.data.data !== 'Incorrect rut and/or Password!' & result.data.data !== 'Please enter rut and Password!' ){
+          console.log('Login correcto!');
+          console.log('Bienvenido',result.data.data)
+          window.IsLogin=true;
+          window.location.href = '/';
+        }
+        else{
+          console.log('Falló la petición2', result.data);
+          //window.location.href = 'http://localhost:3000/login';
+        }
+      } else {
+       console.log('Falló la petición', result.data);
+       //window.location.href = 'http://localhost:3000/login';
+      }
+    })();
   }
-
+  
   return (
     <div className='login'>
       <Sidebar/>
@@ -51,12 +74,12 @@ const Login=()=> {
                 <Avatar style={avatarStyle}> <LockIcon/> </Avatar>
                 <h2>Ingresar</h2>
               </Grid>
-              <TextField label='Rut' placeholder='Enter your rut' fullWidth required name='rut' value={body.rut} onChange={inputChange} style={usuarioStyle}/>
-              <TextField label='Password' placeholder='Enter password' type='password' fullWidth required name='contrasena' value={body.contrasena} onChange={inputChange}/>
+              <TextField label='Rut' placeholder='Enter your rut' fullWidth required name='rut' value={user.rut} onChange={(e) => setRut(e.target.value)} style={usuarioStyle}/>
+              <TextField label='Password' placeholder='Enter password' type='password' fullWidth required name='contrasena' value={user.contrasena} onChange={(e) => setContrasena(e.target.value)}/>
               <FormGroup>
                 <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" />
               </FormGroup>
-              <Button type='submit' color='primary' variant='contained' style={btnstyle} fullWidth onClick={()=> onSubmit(body)} >Sign In</Button>
+              <Button type='submit' color='primary' variant='contained' style={btnstyle} fullWidth onClick={()=> onSubmit()} >Sign In</Button>
             </Paper>
         </Grid>
       </div>
