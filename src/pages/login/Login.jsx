1514beaa81641549d776749eccf1b40a3a8b4bd5
Diego Barros <diego.barros@alumnos.ucn.cl>
2022-07-21 03:40:31 -0400

    cambios diego

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,28 +5,34 @@ import { makeStyles } from '@material-ui/core/styles'
 import Sidebar from '../../components/sidebar/sidebar'
 import axios from 'axios'
 import "./Login.scss"
-import backgroundHome from '../../components/img/backgroundHome.jpg'
+
+import backgroundHome from '../../components/img/bodega.png'
 
 
 const useStyles = makeStyles(theme => ({
   root: {
-      backgroundImage: `url(${backgroundHome})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: '100vh'
+    backgroundImage: `url(${backgroundHome})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    marginLeft: '0',
+    marginBottom: '0',
+    display: 'flex',
   },
   container: {
       height: '60%',
       marginTop: theme.spacing(20),
+      display: 'flex',
       [theme.breakpoints.down(400 + theme.spacing(2) + 2)]: {
-          marginTop: 0,
-          width: '100%',
-          height: '100%'
+          marginTop: -400,
+          marginLeft: 50,
+          width: '80%',
+          height: '60%'
       }
   },
   div: {
-      marginTop: theme.spacing(8),
+      marginTop: theme.spacing(4),
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center'
@@ -46,8 +52,9 @@ const useStyles = makeStyles(theme => ({
 
 
 const Login=()=> {
+  
   const classes = useStyles()
-
+  //const [show, setShow] = useState(true);
   const [rut,setRut]=useState("")
   const [contrasena,setContrasena]=useState("")
   
@@ -81,15 +88,15 @@ const Login=()=> {
           if(localStorage.getItem('isLogin')===null){
             localStorage.setItem('isLogin', '"true"');
             localStorage.setItem('name', names);
+
           }
           window.location.href = '/';
         }
         else{
-
+          localStorage.setItem('nocontra',true);
           window.location.href = 'http://localhost:3000/login';
         }
       } else {
-
        window.location.href = 'http://localhost:3000/login';
       }
     })();
@@ -129,6 +136,13 @@ const Login=()=> {
                           onChange={(e) => setContrasena(e.target.value)}
                           name='password'
                       />
+                      <div>
+                      {localStorage.getItem('nocontra') ? (
+                        <div style={{ color: 'red' }}>Incorrect rut and/or Password!</div>
+                      ) : (
+                        <div></div>
+                      )}
+                      </div>
                       <Button
                           fullWidth
                           variant='contained'
